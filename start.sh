21a7dbffe70e59df5795936ab34453a8d8c2b8f1
Валерий Изгаршев <Walera47@gmail.com>
2021-07-28 21:24:48 +0300

    Fixes

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python manage.py makemigrations reviews
-python manage.py makemigrations titles
-python manage.py makemigrations users
-python manage.py migrate
+python manage.py makemigrations reviews --noinput
+python manage.py makemigrations titles --noinput
+python manage.py makemigrations users --noinput
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
