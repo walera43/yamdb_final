6b59089eadd1a099b27ba14d200e5a4faf460b33
Валерий Изгаршев <Walera47@gmail.com>
2021-07-28 20:05:25 +0300

    Fixes Docker, try collect static

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python manage.py makemigrations reviews
+python manage.py makemigrations titles
+python manage.py makemigrations users
+python manage.py migrate
+python manage.py collectstatic
+CMD gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
