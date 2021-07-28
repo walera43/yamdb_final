96029385aef8b3456dd99aa9d6e16570af93d97d
Валерий Изгаршев <Walera47@gmail.com>
2021-07-28 21:00:39 +0300

    Fix fix Docker-compose

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ python manage.py makemigrations reviews
 python manage.py makemigrations titles
 python manage.py makemigrations users
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
